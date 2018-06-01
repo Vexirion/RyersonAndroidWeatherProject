0c7068b247af8153df6d4e4cce5bf595eb929b8d
Mathew <email>
2018-06-01 16:52:46 -0400

    Added Volley and test OWM data request

--- app/src/main/java/edu/ryerson/mvassair/ryersonandroidweatherapp/MainActivity.java
+++ app/src/main/java/edu/ryerson/mvassair/ryersonandroidweatherapp/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.ryerson.mvassair.ryersonandroidweatherapp;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +7,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,21 +19,46 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout weatherList;
     View separator;
     OWMHandler owm;
+    RequestQueue queue;
+
+    //Example request URL "http://samples.openweathermap.org/data/2.5/weather?id=2172797&units=metric&appid=b6907d289e10d714a6e88b30761fae22"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO: put OWMHandler into a thread
-        owm = new OWMHandler();
+        owm = new OWMHandler(this);
+        queue = Volley.newRequestQueue(this);
 
         weatherLine = findViewById(R.id.WeatherLine);
         weatherList = findViewById(R.id.WeatherList);
         separator = findViewById(R.id.separator);
 
+        owm.makeRequest(getString(R.string.apisample) + getString(R.string.apikey), queue);
+
+
         addNewLines (13);
 
+    }
+
+    //Called by the OWMHandler when a response is gotten
+    protected void sendData(JSONObject data){
+        //Inflate a new line from the template
+        View newline = LayoutInflater.from(this).inflate(R.layout.weatherline, weatherList, false);
+        try {
+            //Get the components of the weather line
+            ImageView weather_image = newline.findViewById(R.id.WeatherImage);
+            TextView line_text = newline.findViewById(R.id.WeatherString);
+            TextView loc_text = newline.findViewById(R.id.Location);
+            //Set the weather line info based on JSON received
+            weather_image.setImageResource(R.drawable.ic_placeholder_cloud);
+            line_text.setText("Current temperature: " + (int)data.getJSONObject("main").getDouble("temp") + getString(R.string.celsius));
+            loc_text.setText(data.getString("name"));
+        }catch(JSONException e){
+            System.out.println("JSON Key not found... probably\n" + e.getMessage());
+        }
+        weatherList.addView(newline);
     }
 
     //test function
--- app/src/main/java/edu/ryerson/mvassair/ryersonandroidweatherapp/OWMHandler.java
+++ app/src/main/java/edu/ryerson/mvassair/ryersonandroidweatherapp/OWMHandler.java
@@ -1,40 +1,91 @@
 package edu.ryerson.mvassair.ryersonandroidweatherapp;
 
-import java.io.*;
-import java.net.*;
-import android.net.*;
+import com.android.volley.*;
+import com.android.volley.toolbox.*;
+
+import org.json.JSONObject;
 
 class OWMHandler {
 
-    URL owmURL;
-    BufferedReader result;
+    JsonObjectRequest req;
+    MainActivity parent;
 
-    public OWMHandler(){
 
-        try{
-            owmURL = new URL("https://www.openweathermap.org");
-            System.out.println("URL made");
+    public OWMHandler(MainActivity parent){
+        req = null;
+        this.parent = parent;
+    }
 
-            try {
-                result = new BufferedReader(new InputStreamReader(owmURL.openStream()));
+    protected void makeRequest(String urlsrc, RequestQueue queue){
 
-                System.out.println("Buffered Reader get");
+        System.out.println(urlsrc);
 
-                String line = "";
-
-                while ((line = result.readLine()) != null)
-                    System.out.print(line);
-
-                result.close();
-            }catch (IOException e){
-                System.out.println("BufferedReader failed. Reason " + e.getMessage() + "\n\n" + e.getStackTrace());
+        //Volley JSON Array request. parameters are: HTTP type, URL to access, JSON to send with request, Response listener, Error listener
+        req = new JsonObjectRequest(Request.Method.GET, urlsrc, null, new Response.Listener<JSONObject>(){
+            public void onResponse(JSONObject response){
+                checkJSON(response);
+                parent.sendData(response);
             }
-        }catch(MalformedURLException e){
-            System.out.println("URL not made. Reason " + e.getMessage() + "\n\n" + e.getStackTrace());
-        }
+        }, new Response.ErrorListener(){
+            public void onErrorResponse (VolleyError error){
+                System.out.println(error.getCause());
+            }
+        });
 
-
+        queue.add(req);
 
     }
 
+    //It looks like Volley already automatically parses all the JSON into dicts and values
+    //So we just have to use them as-is. This function currently exists to be a breakpoint for debugger inspection
+    private void checkJSON(JSONObject JSON){
+
+        if (JSON.has("error"))
+            System.out.println("An HTTP error occurred");
+        else
+            System.out.println("JSON has already been parsed into JSONObjects and appropriate data types");
+    }
+
+
 }
+
+
+/*
+coord
+    coord.lon City geo location, longitude
+    coord.lat City geo location, latitude
+weather (more info Weather condition codes)
+    weather.id Weather condition id
+    weather.main Group of weather parameters (Rain, Snow, Extreme etc.)
+    weather.description Weather condition within the group
+    weather.icon Weather icon id
+base Internal parameter
+main
+    main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    main.pressure Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
+    main.humidity Humidity, %
+    main.temp_min Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    main.temp_max Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    main.sea_level Atmospheric pressure on the sea level, hPa
+    main.grnd_level Atmospheric pressure on the ground level, hPa
+wind
+    wind.speed Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
+    wind.deg Wind direction, degrees (meteorological)
+clouds
+    clouds.all Cloudiness, %
+rain
+    rain.3h Rain volume for the last 3 hours
+snow
+    snow.3h Snow volume for the last 3 hours
+dt Time of data calculation, unix, UTC
+sys
+    sys.type Internal parameter
+    sys.id Internal parameter
+    sys.message Internal parameter
+    sys.country Country code (GB, JP etc.)
+    sys.sunrise Sunrise time, unix, UTC
+    sys.sunset Sunset time, unix, UTC
+id City ID
+name City name
+cod Internal parameter
+*/
